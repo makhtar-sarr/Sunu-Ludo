024843696ebb9796bb31255d639080732c3b3e2b
makhtar-sarr <makhtar.sarr@univ-thies.sn>
2021-11-22 18:42:03 +0000

    seconde commit activity_main.xml

--- app/src/main/java/com/example/sunuludo/MainActivity.kt
+++ app/src/main/java/com/example/sunuludo/MainActivity.kt
@@ -1,16 +1,24 @@
 package com.example.sunuludo
 
-import android.graphics.Rect
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.sunuludo.databinding.ActivityMainBinding
+import com.example.sunuludo.views.OfflineActivity
 
 @Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.offlineButton.setOnClickListener {
+            val offlineActivityIntent = Intent(this, OfflineActivity::class.java)
+            startActivity(offlineActivityIntent)
+        }
 
         window.setFlags(
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
