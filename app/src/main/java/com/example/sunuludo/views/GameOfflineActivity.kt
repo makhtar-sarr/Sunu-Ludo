3b0d10522465ca792dbb316b70531f31b3ed7546
makhtar-sarr <makhtar.sarr@univ-thies.sn>
2021-12-09 07:26:12 +0000

    seconde commit GameOfflineActivity.kt

--- app/src/main/java/com/example/sunuludo/views/GameOfflineActivity.kt
+++ app/src/main/java/com/example/sunuludo/views/GameOfflineActivity.kt
@@ -1,7 +1,9 @@
 package com.example.sunuludo.views
 
 import android.os.Bundle
+import android.view.View.INVISIBLE
 import android.view.WindowManager
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.sunuludo.R
@@ -27,5 +29,39 @@ class GameOfflineActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
+
+        rollDice(binding.diceGreen)
+        binding.diceYellow.visibility = INVISIBLE
+        binding.diceRed.visibility = INVISIBLE
+        binding.diceBlue.visibility = INVISIBLE
+    }
+
+    private fun rollDice(diceImage: ImageView) {
+        // Create new Dice object with 6 sides and roll it.
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        // Find the image in the layout
+
+
+        // determine which drawable resource ID to use based on the dice roll
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.ic_dice_1_fill_svgrepo_com
+            2 -> R.drawable.ic_dice_2_fill_svgrepo_com
+            3 -> R.drawable.ic_dice_3_fill_svgrepo_com
+            4 -> R.drawable.ic_dice_4_fill_svgrepo_com
+            5 -> R.drawable.ic_dice_5_fill_svgrepo_com
+            else -> R.drawable.ic_dice_6_fill_svgrepo_com
+        }
+
+        // Update the imageView with the correct drawable resource ID
+        diceImage.setImageResource(drawableResource)
+    }
+}
+
+class Dice(private val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
     }
 }
